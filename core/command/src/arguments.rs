3ae8be863839557281f396949f1ffca02c209485
andreas <Andreas.Zwander@gmail.com>
2026-08-11 01:47:15 +0200

    Simplify module imports by re-exporting items from `arguments.rs`

--- core/command/src/arguments.rs
+++ core/command/src/arguments.rs
@@ -2,3 +2,8 @@ pub mod argument;
 pub mod argument_builder;
 pub mod arguments;
 pub mod delimited_string;
+
+pub use argument::*;
+pub use argument_builder::*;
+pub use arguments::*;
+pub use delimited_string::*;
