850d6148d521141a6885e234334b06847a8fbbbb
andreas <Andreas.Zwander@gmail.com>
2026-08-10 22:25:11 +0200

    Refactor: Consolidate and clean up modules in core command library

--- core/command/src/lib.rs
+++ core/command/src/lib.rs
@@ -1,17 +1,11 @@
 #![forbid(unsafe_code)]
-#![cfg_attr(not(feature = "std"), no_std)]
-#[cfg(feature = "alloc")]
+
 extern crate alloc;
 // ########################################
 
-pub mod argument;
-pub mod argument_builder;
 pub mod arguments;
 pub mod command;
 pub mod delimited_string;
 
-pub use argument::Argument;
-pub use argument_builder::ArgumentBuilder;
-pub use arguments::Arguments;
 pub use command::Command;
 pub use delimited_string::{DelimitedString, Delimiter};
