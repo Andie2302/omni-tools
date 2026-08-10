dcb9b776a9e4fb4cf1bff8931e6a98d4f298b90b
andreas <Andreas.Zwander@gmail.com>
2026-08-10 20:59:05 +0200

    Refactor core command library to include stability attributes and consolidate module exports

--- core/command/src/lib.rs
+++ core/command/src/lib.rs
@@ -1,5 +1,16 @@
-pub mod argument_builder;
+#![no_std]
+#![forbid(unsafe_code)]
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
 pub mod argument;
-pub mod delimited_string;
+pub mod argument_builder;
 pub mod arguments;
 pub mod command;
+pub mod delimited_string;
+
+pub use argument::Argument;
+pub use argument_builder::ArgumentBuilder;
+pub use arguments::Arguments;
+pub use command::Command;
+pub use delimited_string::{DelimitedString, Delimiter};
