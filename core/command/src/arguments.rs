79bf164cb81ac7f5f86b620c65a8524d7d65e7fd
andreas <Andreas.Zwander@gmail.com>
2026-08-11 21:51:19 +0200

    Refactor: Remove unused modules and re-exports from core argument handling

--- core/command/src/arguments.rs
+++
@@ -1,9 +0,0 @@
-pub mod argument;
-pub mod argument_builder;
-pub mod arguments;
-pub mod delimited_string;
-
-pub use argument::*;
-pub use argument_builder::*;
-pub use arguments::*;
-pub use delimited_string::*;
