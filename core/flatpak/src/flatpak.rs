71a17de63d50f3b0fbbc6396050755c9beccd2f4
andreas <Andreas.Zwander@gmail.com>
2026-08-11 21:52:01 +0200

    Refactor `Flatpak` struct to encapsulate installation logic.

--- core/flatpak/src/flatpak.rs
+++ core/flatpak/src/flatpak.rs
@@ -1,32 +1,28 @@
-use command::{Argument, Arguments, Command};
+pub struct FlatPak {}
 
-pub struct Flatpak;
-
-impl Flatpak {
-    pub fn install(app_id: &str) -> Result<bool, std::io::Error> {
-        let mut args = Arguments::new();
-        args.push(Argument::from("install"));
-        args.push(Argument::from("-y"));
-        args.push(Argument::from(app_id));
-
-        Command::new("flatpak", args).exec_stream_logs()
+impl FlatPak {
+    pub fn new() -> Self {
+        Self {}
     }
-
-    pub fn update() -> Result<bool, std::io::Error> {
-        let mut args = Arguments::new();
-        args.push(Argument::from("update"));
-        args.push(Argument::from("-y"));
-
-        Command::new("flatpak", args).exec_stream_logs()
+    pub fn install(value: String) -> FlatPakInstall {
+        FlatPakInstall::new(value)
     }
-    
-    pub fn uninstall(app_id: &str) -> Result<bool, std::io::Error> {
-        let mut args = Arguments::new();
-        args.push(Argument::from("uninstall"));
-        args.push(Argument::from("-y"));
-        args.push(Argument::from("--delete-data"));
-        args.push(Argument::from(app_id));
+}
 
-        Command::new("flatpak", args).exec_stream_logs()
+pub struct FlatPakInstall {}
+
+impl FlatPakInstall {
+    pub fn new(value: String) -> Self {
+        Self {
+            
+        }
+    }
+        
+}
+
+impl FlatPakInstall {
+    pub fn build()
+    {
+        
     }
 }
