4faab1bd5d85b67e1fc21dccc70bac0dca39081a
andreas <Andreas.Zwander@gmail.com>
2026-08-12 03:27:02 +0200

    refactor(core/command): split `Argument` into separate modules based on feature flags

--- core/command/src/base.rs
+++ core/command/src/base.rs
@@ -1,7 +1,13 @@
 pub mod affix;
 pub mod argument;
 pub mod key_value;
-
 pub use affix::*;
 pub use argument::*;
 pub use key_value::*;
+
+
+#[cfg(feature = "alloc")]
+pub mod argument_alloc;
+
+#[cfg(feature = "alloc")]
+pub use argument_alloc::*;
